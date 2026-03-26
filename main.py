bfec16f4b569a80955c5a11f73e05087f19857fc
Aiden Rathsam <rathsamaiden@gmail.com>
2026-03-26 11:11:10 -0400

    installed flask and added boiler plate code

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine, text, inspect
+
+app = Flask(__name__)
+
+conn_str = 'mysql://root:cset155@localhost/examdb'
+engine = create_engine(conn_str, echo=True)
+conn = engine.connect()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
