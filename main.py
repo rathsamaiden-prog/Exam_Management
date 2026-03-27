625e0252a7fd47831f003ad491e92502c3026083
Aiden Rathsam <rathsamaiden@gmail.com>
2026-03-27 11:15:26 -0400

    Began sign in/log in page :: needs final touches and log in version. Missing query as well

--- main.py
+++ main.py
@@ -11,5 +11,12 @@ conn = engine.connect()
 def index():
     return render_template('index.html')
 
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        return
+    else:
+        return
+
 if __name__ == '__main__':
     app.run(debug=True)
